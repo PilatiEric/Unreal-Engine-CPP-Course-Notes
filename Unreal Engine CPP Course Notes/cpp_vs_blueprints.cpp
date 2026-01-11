16f21bcc707330d4ddb6c5db0a9a98fa7d8cce5d
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-01-10 20:06:48 -0500

    New Notes - C++ VS. Blueprints

--- Unreal Engine CPP Course Notes/cpp_vs_blueprints.cpp
+++ Unreal Engine CPP Course Notes/cpp_vs_blueprints.cpp
@@ -2,4 +2,31 @@
 	C++ VS. Blueprint Notes
 	----------------------------------------------------------------------------
 
+	- C++ is the general purpose programming language
+	- Blueprint is the Visual Scripting System that is built into the Unreal Engine
+
+	-They compliment each other and meant to be used together
+
+	Blueprint Strengths
+	-------------------------
+	- Easier to learn
+	- Programmed inside of Unreal Engine
+	- Quick to make changes and good for prototyping
+		- Gets messy as you build bigger games though
+	- Writing gameplay code
+
+
+	C++ Strengths
+	-------------------------
+	- Gives full control of everything in Unreal Engine (and beyond)
+		- Can use other people's libraries, not just Unreal
+	- Much fast than blueprint
+	- More concise
+	- Better for writing bigger game and non-gameplay code
+
+
+
+
+	- Blueprints can access the C++ code
+		- C++ can't access the blueprints
 */
