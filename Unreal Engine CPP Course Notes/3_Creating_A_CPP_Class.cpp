8dc6118516a69739e132113f260268b7e829d151
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-01-11 18:37:17 -0500

    New Notes - Creating a C++ Class New Document - Important Functions

---
+++ Unreal Engine CPP Course Notes/3_Creating_A_CPP_Class.cpp
@@ -0,0 +1,35 @@
+/*
+	Creating a C++ Class Notes
+
+	SOURCE: https://www.udemy.com/course/unrealcourse/learn/lecture/51309811#overview
+
+	---------------------------------------------------------------------------------------------------------------------------------
+
+	HOW TO START CREATING A CLASS
+	- Click on the "Tools" tab at the top of the screen and select "New C++ Class"
+	- This will pop up a window that let's you choose the parent class
+		- None
+		- Character
+		- Pawn
+		- Actor
+		- Actor Component
+		- Scene Component
+		- Player Camera Manager
+		- Player Controller
+		- Game Mode Base
+		- World Settings
+		- HUD
+		- Player State
+		- Game State Base
+		- Blueprint Function Library
+		- Slate Widget
+		- Slate Widget Style
+		- Unreal Interface
+
+	WHEN A CLASS OBJECT IS FIRST ADDED TO UNREAL
+	- It will not have a form
+	- Does not have a Transform
+		- It does not have any components yet
+			-Components are listed in the details panel on the right side
+
+*/
---
+++ Unreal Engine CPP Course Notes/Important_Functions.cpp
@@ -0,0 +1,10 @@
+/*
+	IMPORTANT FUNCTIONS
+	-----------------------------------------------------------------------------
+
+
+	UE_Log 
+	- Allows log messages to be outputed in the Output Log in Unreal Engine by placing it inside of a function for a certain action
+
+	UE_LOG(LogTemp, Display, TEXT("Hello world!"));
+*/
