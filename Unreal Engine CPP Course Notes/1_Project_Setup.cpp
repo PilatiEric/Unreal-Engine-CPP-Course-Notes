a9c74fd551f48d8804b65a72f789c6cc6b70a940
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-01-10 23:40:09 -0500

    New Notes - Project Setup

---
+++ Unreal Engine CPP Course Notes/1_Project_Setup.cpp
@@ -0,0 +1,36 @@
+/*
+	Project Setup Notes
+	-----------------------------------------------------------------------------
+	Tabs at the Bottom
+	- Content Drawer
+		- Looks like the File Explorer on the computer
+		- Shows all the files in the project
+		- On the left side
+			- Shows the various folder and the hierarchy of what folders they are in
+		- On the right side
+			- Shows the folders on the level of the hierarchy you're at
+		- Actions and Functions
+			- Create new folder
+				- Right click anywhere in there and select "New Folder"
+				- COURSE INFORMATION
+					- We created a folder called "MyStuff" and that's where all assets are being stored
+
+	Create New Level
+	- Click on "File" tab at the top of the screen
+	- Click "New Level"
+	- Brings up a window of different choices
+		- COURSE INFORMATION
+			- We chose "Basic"
+	- Saving Level into Content
+		- Go to File tab and click on "Save Current Level"
+		- Making it the default map
+			- Click on Edit tab at the top of the screen
+			- Click on Project Settings
+			- Click on "Maps & Modes" on the left side under the "Project" section
+			- Changing "Editor Starup Map" and "Game Startup Map"
+			- Change the dropdown to the map you want
+	
+
+
+
+*/
