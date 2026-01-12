edd17c55a45c276bf65abe79402d5521abc9d29f
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-01-11 19:19:50 -0500

    New Notes - Live Coding

---
+++ Unreal Engine CPP Course Notes/4_Live_Coding.cpp
@@ -0,0 +1,26 @@
+/*
+	Live Coding Notes
+
+	SOURCE: https://www.udemy.com/course/unrealcourse/learn/lecture/51309815#overview
+
+	--------------------------------------------------------------------------------------------------------------------------------------
+
+	Live coding let's us compile code without exiting the editor or using Visual Studio
+
+	HOW TO START LIVE CODING
+	- There's a button at the bottom of the Unreal Engine screen towards the right that looks like a bunch of blocks stacked on each other with more floating towards it
+		- Next to the "All Saved" words
+	- Make a change in your code in Visual Studio, save it, then press this button
+	- A box should pop up and should tell you that Live Coding Succeeded
+		- If there is an error in your code
+			- You'll get a red message in the Live Coding window that says "Build failed."
+
+
+
+	RULES OF THUMB
+	- Best used only when making changes to .cpp files
+		- Not .h files
+			- You should close Unreal Engine and rebuild the solution in Visual Studio for these
+	- Recompile your C++ solution at the start or end of your work session
+
+*/
