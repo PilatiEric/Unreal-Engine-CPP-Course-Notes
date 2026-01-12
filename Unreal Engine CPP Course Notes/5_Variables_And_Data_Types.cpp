9b7589d4068d5353eca6c88f91a11c2c5eb911a6
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-01-11 19:52:07 -0500

    New Notes - Variables and Data Types

---
+++ Unreal Engine CPP Course Notes/5_Variables_And_Data_Types.cpp
@@ -0,0 +1,24 @@
+/*
+	Variables and Data Types Notes
+
+	SOURCE: https://www.udemy.com/course/unrealcourse/learn/lecture/51309821#overview
+
+	----------------------------------------------------------------------------------------------------------------------
+
+	Basic Data Types
+	- int
+		- Whole numbers
+	- float
+		- Floating point numbers (has decimals)
+	- double
+		- The same as floats, but can store largers number (more numbers before and after decimal)
+	- char
+		- Single character
+		- Uses single quotes
+	- bool
+		- True or False
+	- FString
+		- Specific to Unreal Engine
+		- Stores a string of characters
+		- Uses double quotes
+*/
