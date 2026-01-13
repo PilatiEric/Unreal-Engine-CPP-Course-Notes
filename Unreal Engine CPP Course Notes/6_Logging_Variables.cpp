1461e37b93e5f08134c217ee0470f5ac1b75ebcc
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-01-12 20:47:09 -0500

    New Notes - Logging Variables Updated Document - Important Functions

---
+++ Unreal Engine CPP Course Notes/6_Logging_Variables.cpp
@@ -0,0 +1,9 @@
+/*
+	Logging Variables Notes
+
+	SOURCE: https://www.udemy.com/course/unrealcourse/learn/lecture/51977811#overview
+
+	-------------------------------------------------------------------------------------------------------------------------
+
+	All notes for this one were put in the Important_Function.cpp under the UE_LOG function section
+*/
