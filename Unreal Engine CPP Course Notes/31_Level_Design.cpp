3d6a98d6b9f0c6d5fb2cca94f8e372cee9c40f9f
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-02-04 22:09:21 -0500

    New Notes - Level Design

---
+++ Unreal Engine CPP Course Notes/31_Level_Design.cpp
@@ -0,0 +1,20 @@
+/*
+	Level Design Notes
+
+	SOURCE: https://www.udemy.com/course/unrealcourse/learn/lecture/51310017#questions/24069835
+
+	------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	HOW TO START THE GAME FROM A DIFFERENT PLACE IN THE LEVEL
+	- Right click on any surface and click "Play From Here"
+	- WARNING: If there are spaces in an object that let's you see behind it, CLICK ON THE OBJECT, NOT THE SPACES
+		- If you click on the spaces, you'll appear behind the object on whatever surface you clicked on
+
+	FOR FIGURING OUT HOW FAR TO SET A MOVING OBJECT TO GO
+	- Look at where the Location is in the details panel
+	- Drag the object to where you want it to go and record the difference between the two Locations
+	- Use that as the distance
+
+	FOR MAKING IT EASIER TO PLACE OBJECT NEAR WHERE A MOVING OBJECT WILL GO
+	- Take another object and stick it next to where the moving object is before returning the moving object to its starting position
+*/
