9d3410eab7013c148b3563a280746a6f5fd09b63
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-02-05 22:56:05 -0500

    New Notes - Packaging the Game

---
+++ Unreal Engine CPP Course Notes/32_Packaging_The_Game.cpp
@@ -0,0 +1,18 @@
+/*
+	Packaging the Game Notes
+
+	SOURCE: https://www.udemy.com/course/unrealcourse/learn/lecture/51310023#questions/24069835
+
+	------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	HOW TO PACKAGE THE GAME
+	- Above the level screen on the right side is a dropdown menu called "Platforms"
+	- Click on it
+	- Go to the operating system you want to package it for
+		- We're using Windows
+	- Under the "BINARY CONFIGURATION" click "Shipping"
+		- Do this only if you're done developing
+		- If you are not, leave "Use Project Setting (Development)" selected
+	- Click "Package Project"
+	- Select where you want to save the build
+*/
