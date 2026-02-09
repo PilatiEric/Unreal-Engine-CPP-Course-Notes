c77c8713fb85b6b0ec57f4d3319d521e9647453f
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-02-09 18:40:17 -0500

    New Notes - Creating the Project

---
+++ Unreal Engine CPP Course Notes/1_Creating_The_Project.cpp
@@ -0,0 +1,22 @@
+/*
+	Creating The Project Notes
+
+	SOURCE: https://www.udemy.com/course/unrealcourse/learn/lecture/51621441#questions/24088171
+
+	--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	WHEN CREATING AN EMPTY LEVEL
+	- Object will not appear visible if you have this thing set to "Lit"
+	- Above the level window, there are a bunch of buttons along the right side of the top
+	- Find the one in between the Camera with the arrow dropdown (labeled as "Set the camera speed" in the tool tip) and the one with the eyeball (labeled as "show flags related to the current viewpoint" in the tooltip)
+	- The one you're looking for will be labeled "View mode settings for the current viewpoint" in the tooltip
+	- Click it and set it to "Unlit" 
+	- You should be able to see all the items now
+
+	CREATING A PLAYER START POINT
+	- In the "Quickly add to the project" dropdown menu (three dropdown menus to the left of the Play button)
+	- Go to the "Basic" list
+	- Click and drag "Player Start" to the level
+
+
+*/
