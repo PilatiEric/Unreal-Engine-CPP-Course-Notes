118bea00206d56d5d61770b3718fc73ea6833f38
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-02-09 19:34:24 -0500

    New Notes - Materials

---
+++ Unreal Engine CPP Course Notes/3_Materials.cpp
@@ -0,0 +1,42 @@
+/*
+	Materials Notes
+
+	SOURCE: https://www.udemy.com/course/unrealcourse/learn/lecture/51635447#questions/24088171
+
+	-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	WHAT IS A MATERIAL
+	- Determines how a Mesh looks
+
+	HOW TO SET A MATERIAL
+	- Find the material in your Content Browser
+	- Click and drag it to the item you want it applied to
+	- In the item's Details panel, it can be found in the "Surface Materials" section
+		- You can pick a different material from the dropdown list in it as well
+	
+
+	ADDING MORE MATERIALS AND OTHER STUFF AVAILABLE
+	- We're going to get the "Starter Content Asset Pack"
+	- In the Content Browser, while in the Content folder, click "Add"
+	- In the menu, click "Add Feature or Content Pack..."
+	- In the panel that pops up, click "Content" at the top
+		- Is next to "Blueprint" and "C++"
+	- Click "Starter Content"
+	- Click "Add to Project" at the bottom
+	- Once it is done and the "StarterContent" folder is added to the Content folder, close the panel
+
+
+	WHEN APPLYING THE MATERIAL TO A SHAPE
+	- Only the side you dragged the material to will change
+	- To add the material faster, select the material you want, then add the object in
+
+	IF YOU WANT TO HOLLOW OUT AN OBJECT
+	- This requires the object to be a Geometry Brush object
+	- Go into the Details menu
+	- Go down to the "Brush Settings" section
+	- Check the box for "Hollow"
+	- Use the "Wall Thickness" number to determine how thick the walls are
+
+
+
+*/
