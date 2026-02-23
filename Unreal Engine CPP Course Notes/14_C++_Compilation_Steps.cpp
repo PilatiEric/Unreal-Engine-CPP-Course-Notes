8530f55451ab3779071a6bfd241dcf3238ba0489
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-02-22 20:54:28 -0500

    New Notes - C++ Compilation Steps

---
+++ Unreal Engine CPP Course Notes/14_C++_Compilation_Steps.cpp
@@ -0,0 +1,23 @@
+/*
+	C++ Compilation Steps Notes
+
+	SOURCE: https://www.udemy.com/course/unrealcourse/learn/lecture/51746121#overview
+
+	----------------------------------------------------------------------------------------------------------------------------------------------------
+
+	STEPS
+	- Starts at source code
+		- C++ and .h files
+	- Feeds into Unreal Header Tool
+		- Program from Unreal Engine
+		- Looks for Unreal specific syntax and creates more C++ files
+	- Standard C++ Cocpilation
+		- Preprocessor
+			- Looks like preprocessor directives
+				- Like the #include files
+		- Compiler
+		- Linker
+			- Creates all connections and give .exe
+
+
+*/
