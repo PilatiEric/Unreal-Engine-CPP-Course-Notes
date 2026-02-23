97f05901944eb13e614bcd27aa62a71f70e47744
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-02-22 22:23:14 -0500

    New Notes - The Constructor

---
+++ Unreal Engine CPP Course Notes/16_The_Constructor.cpp
@@ -0,0 +1,16 @@
+/*
+	The Constructor Notes
+
+	SOURCE: https://www.udemy.com/course/unrealcourse/learn/lecture/51806169#overview
+
+	--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	WHAT IS THE CONSTRUCTOR
+	- A function that is automatically called for a class when an object of that class is created
+	- We can put any code we want in it
+	- WE WILL LOOK INSIDE THE MOVER COMPONENT FOR THIS
+		- There is a variable being set to true
+			- PrimaryComponentTick.bCanEverTick = true;
+				- The b in bCanEverTick is a naming convention in Unreal to say it's a bool
+
+*/
