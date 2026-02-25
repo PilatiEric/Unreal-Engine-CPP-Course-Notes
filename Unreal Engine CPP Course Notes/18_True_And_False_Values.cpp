541ab8144b339fc27883a589d5803d9fd3d0aa7b
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-02-24 23:12:30 -0500

    New Notes - True and False Values

---
+++ Unreal Engine CPP Course Notes/18_True_And_False_Values.cpp
@@ -0,0 +1,43 @@
+/*
+	True and False Values Notes
+
+	SOURCE: https://www.udemy.com/course/unrealcourse/learn/lecture/51806193#overview
+
+	--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	FACTS ABOUT TRUE AND FALSE IN C++
+	- 0 is used to represent false
+		- EXAMPLES OF THINGS THAT REPRESENT FALSE
+			- false
+			- 0
+			- nullptr
+			- NULL (Used in C, nullptr was introduced in C++)
+	- Everything non-zero is true
+		- EXAMPLES
+			- true
+			- "Hello"
+			- 'a'
+			- 10
+			- 3.45f
+			- -999
+
+
+
+	HOW TO SIMPLIFY CHECKS FOR NULLPTR
+	- Valid pointers have non-zero numbers
+	- Nullptr has false value
+		- Has a 0 value
+
+		AActor* MyActor1 = &OtherActor;		//true
+
+		AActor* MyActor2 = 0;				//false
+		AActor* MyActor3 = nullptr;			//false
+		AActor* MyActor4 = NULL;			//false
+
+	- An if statement check for a nullptr can be simplified as this
+
+		if (MyActor1)	//If it has a valid pointer, this will return true
+
+		if (!MyActor1)	//If it has a valid pointer, this will return false
+
+*/
